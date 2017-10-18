d4fe5b6fbe96b6343e6592245f70f5272e45bf80
Arison <784602719@qq.com>
2017-10-18 08:52:41 +0800

    fix

--- src/main/java/com/gradle/test/http/testHttp.java
+++ src/main/java/com/gradle/test/http/testHttp.java
@@ -1,37 +1,34 @@
 package com.gradle.test.http;
 
 import com.android.base.net.HttpClient;
+import com.android.base.net.HttpClient.Method;
 import com.android.base.net.RetrofitImpl;
+
 import com.android.retrofit.subscriber.NetResquestSubscriber;
 import com.android.retrofit.subscriber.SubscriberOnNextListener;
 
 /**
- * 测试网络请求
- * Created by Arison on 2017/5/31.
+ * 测试网络请求 Created by Arison on 2017/5/31.
  */
 public class testHttp {
-    public static final String BASE_URL="http://192.168.253.200:8080/Chapter/";
-    
-    /**
-      * @desc:
-      * @author：Arison on 2017/5/31
-      */
-    public static void main(String arg[]){
-        HttpClient httpClient=new HttpClient.Builder()
-                .url(BASE_URL)
-                .httpBase(RetrofitImpl.getInstance())
-                .build();
+	public static final String BASE_URL = "http://113.105.74.140:8092/";
 
-
-        httpClient.Api().send(new HttpClient.Builder()
-        .url("json")
-        .add("12","12")
-        //.header("12","12")
-        .build(),new NetResquestSubscriber<>(new SubscriberOnNextListener<Object>() {
-            @Override
-            public void onNext(Object o) {
-
-            }
-        }));
-    }
+	/**
+	 * @desc:
+	 * @author：Arison on 2017/5/31
+	 */
+	public static void main(String arg[]) {
+		HttpClient httpClient = new HttpClient.Builder().url(BASE_URL).isDebug(true).httpBase(RetrofitImpl.getInstance()).build();
+		httpClient.Api()
+				.send(new HttpClient.Builder().url("http://113.105.74.140:8092/user/appStoreList")
+						.add("type", "0")
+						.add("pageIndex", "1")
+						.method(Method.POST)
+						.build(), new NetResquestSubscriber<>(new SubscriberOnNextListener<Object>() {
+							@Override
+							public void onNext(Object o) {
+								System.out.println(o.toString());
+							}
+						}));
+	}
 }
